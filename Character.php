40a645d5c21aed1419a03ee8c7af0897ad40fc58
Koningstein <m.koningstein@tcrmbo.nl>
2025-03-06 09:42:53 +0100

    Opdracht 3: Properties toegevoegd aan Character class

--- Character.php
+++ Character.php
@@ -2,4 +2,10 @@
 
 class Character 
 {
+    public $name;
+    public $health;
+    public $attack;
+    public $defense;
+    public $range;
+    public $role;
 } 
--- index.php
+++ index.php
@@ -3,5 +3,12 @@
 require_once 'Character.php';
 
 $hero = new Character();
+$hero->name = "Eldrin";
+$hero->health = 100;
+$hero->attack = 15;
+$hero->defense = 10;
+$hero->range = 2;
+$hero->role = "Mage";
+
 echo "<pre>";
 var_dump($hero); 
