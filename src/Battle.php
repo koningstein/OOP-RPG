9c3403cc30a2b64f34b90f99462ddff92cdb1d6f
Koningstein <m.koningstein@tcrmbo.nl>
2025-04-17 16:13:05 +0200

    overbodig weg

--- src/Battle.php
+++ src/Battle.php
@@ -117,67 +117,6 @@ class Battle
         $this->maxRounds = $maxRounds;
     }
 
-//    /**
-//     * Starts and manages a fight between two characters
-//     * @param Character $fighter1
-//     * @param Character $fighter2
-//     * @return string
-//     */
-//    public function startFight(Character $fighter1, Character $fighter2): string
-//    {
-//        // Reset battleLog en roundNumber aan het begin van het gevecht
-//        $this->roundNumber = 1;
-//        $this->battleLog = "Battle Start!<br><br>";
-//
-//        // Kopie van originele health-waardes om te kunnen resetten na het gevecht
-//        $fighter1OriginalHealth = $fighter1->getHealth();
-//        $fighter2OriginalHealth = $fighter2->getHealth();
-//
-//        while (($fighter1->getHealth() > 0 && $fighter2->getHealth() > 0) && ($this->roundNumber <= $this->maxRounds)) {
-//            $this->battleLog .= "Round " . $this->roundNumber . ":<br>";
-//
-//            // Fighter 1's turn
-//            $damage = $this->calculateDamage($fighter1, $fighter2);
-//            $fighter2->takeDamage($damage);
-//            $this->battleLog .= $fighter1->getName() . " hits " . $fighter2->getName() . " for " . $damage . " damage!<br>";
-//            $this->battleLog .= $fighter2->getName() . " has " . $fighter2->getHealth() . " health remaining.<br>";
-//
-//            if ($fighter2->getHealth() <= 0) {
-//                $this->battleLog .= "<br>" . $fighter1->getName() . " wins the battle!";
-//
-//                // We resetten de health waarden NIET meer hier
-//                // De originele health waarden worden later gebruikt in de template
-//
-//                return $this->battleLog;
-//            }
-//
-//            // Fighter 2's turn
-//            $damage = $this->calculateDamage($fighter2, $fighter1);
-//            $fighter1->takeDamage($damage);
-//            $this->battleLog .= $fighter2->getName() . " hits " . $fighter1->getName() . " for " . $damage . " damage!<br>";
-//            $this->battleLog .= $fighter1->getName() . " has " . $fighter1->getHealth() . " health remaining.<br><br>";
-//
-//            if ($fighter1->getHealth() <= 0) {
-//                $this->battleLog .= "<br>" . $fighter2->getName() . " wins the battle!";
-//
-//                // We resetten de health waarden NIET meer hier
-//                // De originele health waarden worden later gebruikt in de template
-//
-//                return $this->battleLog;
-//            }
-//
-//            $this->roundNumber++;
-//        }
-//
-//        // Maximum rounds reached without a winner
-//        $this->battleLog .= "Maximum rounds (" . $this->maxRounds . ") reached! It's a draw!";
-//
-//        // We resetten de health waarden NIET meer hier
-//        // De originele health waarden worden later gebruikt in de template
-//
-//        return $this->battleLog;
-//    }
-
     /**
      * @return string[]
      */
